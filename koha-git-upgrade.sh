876a7b3fe898fdc6e8fcfba9d071b979fd3d4c09
Chris Cormack <chris@bigballofwax.co.nz>
2008-05-15 22:17:58 +1200

    Adding license information And now checking to see if a database upgrade is needed

--- koha-git-upgrade.sh
+++ koha-git-upgrade.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright 2008 
+# Chris Cormack chris@bigballofwax.co.nz
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#	    
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#			    
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ $# -ne 3 ]; then
   echo 1>&2 Usage: $0 path/to/git/repo path/to/koha-install-log opacurl
   exit 127
@@ -26,8 +42,14 @@ if git fetch; then
   fi
 fi
 
+
 if make install; then
-  wget --delete-after $3 2>&1 | grep "/cgi-bin/koha/maintenance.pl"
+  if wget --delete-after $3 2>&1 | grep "/cgi-bin/koha/maintenance.pl"; then
+    echo "Database upgrade needed";
+    exit 127
+  else
+    echo "Upgrade finished";
+  fi
 else
   echo "Can't install";
   exit 127
