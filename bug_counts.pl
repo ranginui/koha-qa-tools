b3c98f79746f3fc5c2e94ec00db4d9b331492e18
Chris Cormack <chrisc@catalyst.net.nz>
2011-04-15 12:13:36 +1200

    Adding caching

--- bug_counts.pl
+++ bug_counts.pl
@@ -22,27 +22,39 @@ use Text::CSV;
 use warnings;
 use strict;
 use CGI;
+use File::Cache;
 
+my $cache = new File::Cache( { namespace  => 'MyCache',
+				                                 expires_in => 3600,
+				                                 filemode => 0600 } );
 my $query = new CGI;
-my $csv   = Text::CSV->new;
-
-my $content = get(
-"http://bugs.koha-community.org/bugzilla3/report.cgi?bug_file_loc=&bug_file_loc_type=allwordssubstr&bug_id=&bug_id_type=anyexact&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&chfield=cf_patch_status&chfieldfrom=&chfieldto=&chfieldvalue=&deadlinefrom=&deadlineto=&email1=&email2=&email3=&emailassigned_to1=1&emailassigned_to2=1&emailcc2=1&emailqa_contact2=1&emailreporter2=1&emailtype1=substring&emailtype2=substring&emailtype3=substring&field0-0-0=noop&keywords=&keywords_type=allwords&longdesc=&longdesc_type=allwordssubstr&short_desc=&short_desc_type=allwordssubstr&type0-0-0=noop&value0-0-0=&x_axis_field=cf_patch_status&y_axis_field=version&z_axis_field=&width=600&height=350&action=wrap&ctype=csv&format=table"
-);
+my $csv = Text::CSV->new;
+my $content = $cache->get('content');
+if (! $content){
+    my $content=get("http://bugs.koha-community.org/bugzilla3/report.cgi?bug_file_loc=&bug_file_loc_type=allwordssubstr&bug_id=&bug_id_type=anyexact&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&chfield=cf_patch_status&chfieldfrom=&chfieldto=&chfieldvalue=&deadlinefrom=&deadlineto=&email1=&email2=&email3=&emailassigned_to1=1&emailassigned_to2=1&emailcc2=1&emailqa_contact2=1&emailreporter2=1&emailtype1=substring&emailtype2=substring&emailtype3=substring&field0-0-0=noop&keywords=&keywords_type=allwords&longdesc=&longdesc_type=allwordssubstr&short_desc=&short_desc_type=allwordssubstr&type0-0-0=noop&value0-0-0=&x_axis_field=cf_patch_status&y_axis_field=version&z_axis_field=&width=600&height=350&action=wrap&ctype=csv&format=table");
+    $cache->set('content',$content);
+}
 
 print $query->header;
-
-
-my @lines = split( /\n/, $content );
+#print $query->start_html;
+#print $content;
+my @lines = split (/\n/,$content);
 pop @lines;
-print "<h3>Patch statuses</h3>\n";
-print "<table cellpadding=\"2\">\n";
-print
-"<tr><th>Version</th><th>Needs Signoff</th><th>Signed Off</th><th>Pushed</th><th>Failed QA</th><th>Does not apply</th></tr>\n";
-foreach my $line (@lines) {
-    my $parsed  = $csv->parse($line);
+print "<h3>Patch statuses</h3>";
+print "<h5>Updated hourly</h5>";
+    print "<table cellpadding=\"2\">";
+    print "<tr><th>Version</th><th>Needs Signoff</th><th>Signed Off</th><th>Pushed</th><th>Failed QA</th><th>Does not apply</th></tr>";
+foreach my $line (@lines){
+    my $parsed = $csv->parse($line);
     my @columns = $csv->fields();
-    print "<tr><td>$columns[0]</td><td>$columns[4]</td><td>$columns[7]</td>
-        <td>$columns[5]</td><td>$columns[3]</td><td>$columns[2]</td></tr>\n";
-}
-print "</table>"\n;
+
+#    if ($columns[0] eq 'rel_3_4'){
+#	print "<h4>Bug Counts for 3.4</h4>";
+
+	print "<tr><td>$columns[0]</td><td>$columns[4]</td><td>$columns[7]</td>
+        <td>$columns[5]</td><td>$columns[3]</td><td>$columns[2]</td></tr>";
+
+#    }
+ 
+    }
+   print "</table>";
